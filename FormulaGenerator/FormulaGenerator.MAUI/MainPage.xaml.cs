fac7b0151b86f572b4720501402f8fad916957b4
Jim.Jiang <jim.jiang@ringcentral.com>
2022-07-09 16:00:00 +0800

    first commit.

---
+++ FormulaGenerator/FormulaGenerator.MAUI/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Maui.Graphics.Skia;
+
+namespace FormulaGenerator.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.Photos>();
+
+            // Create a bitmap in memory and draw on its Canvas
+            SkiaBitmapExportContext bmp = new(600, 400, 1.0f);
+            ICanvas canvas = bmp.Canvas;
+
+            // Draw a big blue rectangle with a dark border
+            Rect backgroundRectangle = new(0, 0, bmp.Width, bmp.Height);
+            canvas.FillColor = Color.FromArgb("#003366");
+            canvas.FillRectangle(backgroundRectangle);
+            canvas.StrokeColor = Colors.Black;
+            canvas.StrokeSize = 20;
+            canvas.DrawRectangle(backgroundRectangle);
+
+            // Draw circles randomly around the image
+            for (int i = 0; i < 100; i++)
+            {
+                float x = Random.Shared.Next(bmp.Width);
+                float y = Random.Shared.Next(bmp.Height);
+                float r = Random.Shared.Next(5, 50);
+
+                Color randomColor = Color.FromRgb(
+                    red: Random.Shared.Next(255),
+                    green: Random.Shared.Next(255),
+                    blue: Random.Shared.Next(255));
+
+                canvas.StrokeSize = r / 3;
+                canvas.StrokeColor = randomColor.WithAlpha(.3f);
+                canvas.DrawCircle(x, y, r);
+            }
+
+            // Measure a string
+            string myText = "Hello, Maui.Graphics!";
+            Microsoft.Maui.Graphics.Font myFont = new Microsoft.Maui.Graphics.Font("Impact");
+            float myFontSize = 48;
+            canvas.Font = myFont;
+            SizeF textSize = canvas.GetStringSize(myText, myFont, myFontSize);
+
+            // Draw a rectangle to hold the string
+            Point point = new(
+                x: (bmp.Width - textSize.Width) / 2,
+                y: (bmp.Height - textSize.Height) / 2);
+            Rect myTextRectangle = new(point, textSize);
+            canvas.FillColor = Colors.Black.WithAlpha(.5f);
+            canvas.FillRectangle(myTextRectangle);
+            canvas.StrokeSize = 2;
+            canvas.StrokeColor = Colors.Yellow;
+            canvas.DrawRectangle(myTextRectangle);
+
+            // Daw the string itself
+            canvas.FontSize = myFontSize * .9f; // smaller than the rectangle
+            canvas.FontColor = Colors.White;
+            canvas.DrawString(myText, myTextRectangle,
+                HorizontalAlignment.Center, VerticalAlignment.Center, TextFlow.OverflowBounds);
+
+            // Save the image as a PNG file
+            var picDir = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            var picPath = Path.Combine(picDir, "console2.png");
+            bmp.WriteToFile(picPath);
+        }
+    }
+}
---
+++ FormulaGenerator/FormulaGenerator/Formula.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaGenerator
+{
+    public class Formula
+    {
+        public enum MathSymbol
+        {
+            Plus,
+            Minus,
+            Multiplied,
+            Divided,
+            Unknow
+        }
+
+        private static Random random = new Random();
+        public static int Random_0_10 => random.Next(0, 10);
+        public static int Random_3_10 => random.Next(3, 10);
+        public static int Random_10_100 => random.Next(10, 100);
+        public static int Random_30_100 => random.Next(30, 100);
+        public static int Random_NN0 => Random_30_100 * 10;
+        public static int Random_N0N => Random_3_10 * 100 + Random_3_10;
+        public static int Random_10_50 => random.Next(10, 50);
+        public static int Random_50_100 => random.Next(50, 100);
+        public static int Random_3_100 => random.Next(3, 100);
+
+        public static Formula Plus => (Formula)Random_10_100 + (Formula)Random_10_100;
+        public static Formula Minus => (Formula)Random_50_100 - (Formula)Random_10_50;
+        public static Formula Multiplied => (Formula)Random_30_100 * (Formula)Random_30_100;
+        public static Formula Multiplied1 => (Formula)Random_NN0 * (Formula)Random_3_10;
+        public static Formula Multiplied2 => (Formula)Random_N0N * (Formula)Random_3_10;
+        public static Formula SimpleMultiplied => (Formula)Random_3_10 * (Formula)Random_3_10;
+        public static Formula Divided => (Formula)Random_50_100 / (Formula)Random_3_10;
+        public static Formula SimpleDivided => (Formula)Random_3_10 / (Formula)Random_3_10;
+        public static Formula SimpleRandomMathSymbol(Formula formula1, Formula formula2)
+        {
+            var randomNum = random.Next(0, 4);
+            return randomNum switch
+            {
+                0 => formula1 + formula2,
+                1 => formula1 - formula2,
+                2 => formula1 * formula2,
+                3 => formula1 / formula2,
+                _ => formula1 + formula2,
+            };
+        }
+
+        public int Value { get; }
+        public Formula? Formula1 { get; }
+        public MathSymbol? Symbol { get; }
+        public Formula? Formula2 { get; }
+        public int FormulaResult
+        {
+            get
+            {
+                if (Symbol == null || Formula1 == null || Formula2 == null)
+                {
+                    return Value;
+                }
+                return Symbol switch
+                {
+                    MathSymbol.Plus => Formula1.FormulaResult + Formula2.FormulaResult,
+                    MathSymbol.Minus => Formula1.FormulaResult - Formula2.FormulaResult,
+                    MathSymbol.Multiplied => Formula1.FormulaResult * Formula2.FormulaResult,
+                    MathSymbol.Divided => Formula1.FormulaResult / Formula2.FormulaResult,
+                    _ => Value,
+                };
+            }
+        }
+
+        public Formula(int value)
+        {
+            Value = value;
+        }
+        public Formula(int value1, MathSymbol symbol, int value2) : this((Formula)value1, symbol, (Formula)value2)
+        {
+        }
+        public Formula(Formula formula1, MathSymbol symbol, Formula formula2)
+        {
+            Formula1 = formula1;
+            Symbol = symbol;
+            Formula2 = formula2;
+        }
+        public Formula(int value1, int value2)
+        {
+            Formula1 = (Formula)(value1 * value2);
+            Symbol = MathSymbol.Divided;
+            Formula2 = (Formula)value2;
+        }
+
+        public string WithBrackets()
+        {
+            if (Symbol == null || Formula1 == null || Formula2 == null)
+            {
+                return Value.ToString();
+            }
+            var result = Symbol switch
+            {
+                MathSymbol.Plus => $"({Formula1} + {Formula2})",
+                MathSymbol.Minus => $"({Formula1} - {Formula2})",
+                MathSymbol.Multiplied => $"{Formula1} × {Formula2}",
+                MathSymbol.Divided => $"{Formula1} ÷ {Formula2}",
+                _ => ToString()
+            };
+            return result;
+        }
+
+        public override string ToString()
+        {
+            if (Symbol == null || Formula1 == null || Formula2 == null)
+            {
+                return Value.ToString();
+            }
+            return Symbol switch
+            {
+                MathSymbol.Plus => $"{Formula1} + {Formula2}",
+                MathSymbol.Minus => $"{Formula1} - {Formula2}",
+                MathSymbol.Multiplied => $"{Formula1.WithBrackets()} × {Formula2.WithBrackets()}",
+                MathSymbol.Divided => $"{Formula1.WithBrackets()} ÷ {Formula2.WithBrackets()}",
+                _ => ToString()
+            };
+        }
+
+        public static explicit operator int(Formula fcormula)
+        {
+            return fcormula.FormulaResult;
+        }
+
+        public static explicit operator Formula(int value)
+        {
+            return new Formula(value);
+        }
+
+        public static Formula operator +(Formula formula1, Formula formula2)
+        {
+            return new Formula(formula1, MathSymbol.Plus, formula2);
+        }
+        public static Formula operator -(Formula formula1, Formula formula2)
+        {
+            return new Formula(formula1, MathSymbol.Minus, formula2);
+        }
+        public static Formula operator *(Formula formula1, Formula formula2)
+        {
+            return new Formula(formula1, MathSymbol.Multiplied, formula2);
+        }
+        public static Formula operator /(Formula formula1, Formula formula2)
+        {
+            return new Formula((Formula)((int)formula1 * (int)formula2), MathSymbol.Divided, formula2);
+        }
+    }
+}
